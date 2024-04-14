dae59a09093612e05d77c3eaf2de5c82be2b35c7
hiroky1983 <hirockysan1983@gmail.com>
2024-04-14 13:20:46 +0900

    チュートリアル

--- backend/graph/schema.resolvers.go
+++ backend/graph/schema.resolvers.go
@@ -6,18 +6,26 @@ package graph
 
 import (
 	"context"
+	"math/rand"
+
 	"fmt"
 	"go-graph/graph/model"
 )
 
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+	todo := &model.Todo{
+		ID:   fmt.Sprintf("T%d", rand.Int()),
+		Text: input.Text,
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+		r.todos = append(r.todos, todo)
+		return todo, nil
 }
 
 // Todos is the resolver for the todos field.
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+	return r.todos, nil
 }
 
 // Mutation returns MutationResolver implementation.
